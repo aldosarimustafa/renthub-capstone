f4e43ee665c131b2a665863841c90975232488ce
aldosarimustafa <84543087+aldosarimustafa@users.noreply.github.com>
2026-06-05 20:43:31 -0400

    Configure React routing and basic pages

---
+++ renthub-client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>RentHub</h1>
+            <p>Find your next rental property.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ renthub-client/src/pages/Properties.jsx
@@ -0,0 +1,9 @@
+function Properties() {
+    return (
+        <div>
+            <h1>Available Properties</h1>
+        </div>
+    );
+}
+
+export default Properties;
