e598d2c04a2af2a29ea2537e7501486e1587d34d
aldosarimustafa <84543087+aldosarimustafa@users.noreply.github.com>
2026-06-07 16:52:59 -0400

    Configure authentication routes

--- renthub-server/server.js
+++ renthub-server/server.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const User = require("./models/User");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("RentHub API is running");
