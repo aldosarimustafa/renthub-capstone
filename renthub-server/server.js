9b168f168e8bc65bb229d685c41ce4eadb008f8a
aldosarimustafa <84543087+aldosarimustafa@users.noreply.github.com>
2026-06-07 16:47:49 -0400

    Implement user model

--- renthub-server/server.js
+++ renthub-server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
