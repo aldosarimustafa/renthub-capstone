d68d3396cc3ff6df44b8525fcdf94397b9db429c
aldosarimustafa <84543087+aldosarimustafa@users.noreply.github.com>
2026-06-08 18:09:48 -0400

    Connect lease routes to server

--- renthub-server/server.js
+++ renthub-server/server.js
@@ -10,6 +10,7 @@ const applicationRoutes = require("./routes/applicationRoutes");
 const maintenanceRoutes = require("./routes/maintenanceRoutes");
 const leaseRoutes = require("./routes/leaseRoutes");
 
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
